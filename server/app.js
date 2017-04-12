0d58a15e91b7835809d42c129b18a5a759b6a521
Administrator <administrator@Administrators-MacBook-Pro.local>
2017-04-12 14:07:28 -0400

    Server is running and dependencies are installed

--- server/app.js
+++ server/app.js
@@ -1,2 +1,17 @@
 
-console.log('Hello World');
+
+const express = require('express');
+
+let app = express();
+
+app.get('/', function showHome(req, res, next) {
+    console.log(request.url);
+
+    response.status(200);
+    response.setHeader('Content-Type', 'text/html');
+    response.end('<h1>Hello World</h1>');
+});
+
+app.listen(3000, function ServerIsUp() {
+    console.log('The server is up!');
+});
