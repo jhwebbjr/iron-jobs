061c67f22b1461fd17ea0054179ab80930c8eadb
Administrator <administrator@Administrators-MacBook-Pro.local>
2017-04-13 14:36:37 -0400

    MongoDB and mongoose are up and running. We are getting into the server location

--- server/app.js
+++ server/app.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser');
 let app = express();
 
 app.use(bodyParser.json());
+console.log('database MY_DB_LOCATION', process.env.MY_DB_LOCATION);
+console.log('database location', process.env);
+
 
 app.use(express.static('./client/public/'));
 
