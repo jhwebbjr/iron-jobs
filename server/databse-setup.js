47a6cb4d92f57bd881693129f681fd32bc01d25e
Administrator <administrator@Administrators-MacBook-Pro.local>
2017-04-13 14:37:21 -0400

    Setup the databse

---
+++ server/databse-setup.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+if (!process.env.MY_DB_LOCATION) {
+    console.error('No database detected');
+    process.exit(128);
+}
+
+mongoose.connect(process.env.MY_DB_LOCATION);
+
+mongoose.connection.on('error', function handleDBErrors(err) {
+    console.error('DB Error', err);
+    process.exit(128);
+
+});
